54bfef274ae6e106ce46dc932a7eda4fc68e0fa5
itqanpos <mohamedalsonbaty673@gmail.com>
2026-02-22 23:04:01 +0200

    Add AppColors class with color constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF0D6EFD);
+  static const background = Color(0xFF0D1117);
+  static const card = Color(0xFF161B22);
+}
